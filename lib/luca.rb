f3732eeefe452533be42fdf98eb84f85672c910c
Jerome Lacoste <jerome.lacoste@gmail.com>
2024-03-18 08:13:30 +0100

    Remove unusued code

--- lib/luca.rb
+++ lib/luca.rb
@@ -15,28 +15,6 @@ module Luca
 			@access_token = @env['personal_token']
 		end
 
-		#def login(username, password)
-		def authenticate
-			uri = 'https://authentication.wolt.com/v1/wauth2/access_token'
-			refresh_token = 'f283d94c-adcc-4f98-bb0f-8ba0179f7295'
-
-			form = {
-				grant_type: "refresh_token",
-				refresh_token: refresh_token
-			}
-
-			response = self.class.post(uri, headers: {
-				'Accept' => 'application/json, text/plain, */*',
-				'Accept-Language' => 'en-US,en;q=0.5',
-				'Connection' => 'keep-alive',
-				'Content-Type' => 'application/x-www-form-urlencoded',
-			}, body: form)
-
-			json = JSON.parse(response.body)
-			@access_token = json['access_token']
-			@refresh_token = json['refresh_token']
-		end
-
 		def customers
 			body = {
 				'query' => 'query { saleCustomers { nodes { id , active, comment, email, id, name } } }',
@@ -133,36 +111,6 @@ module Luca
 			response = self.class.post(self.class.base_uri, body: body.to_json, headers: headers)
 			json = JSON.parse(response.body)
 		end
-		def timestr(time)
-			Time.parse(time).iso8601(3)
-		end
-
-# external code, for pagination example
-# /v1/merchant-admin/venues/605e20a324012913ebbb649d/completed_purchases?end_time=2021-10-03T00:00:00.000+02:00&limit=40&start_time=2021-10-02T00:00:00.000+02:00
-=begin
-	  	def purchases(venue_id:, start_time:, end_time:, limit: 40)
-			uri = "/merchant-admin/venues/#{venue_id}/completed_purchases"
-			params = {
-				start_time: timestr(start_time),
-				end_time: timestr(end_time),
-				limit: limit
-			}
-			purchases = []
-			loop do
-				response = self.class.get(uri, headers: headers, query: params)
-				json = JSON.parse(response.body)
-				#puts json
-				purchases += json['data']['purchases']
-
-				cursor_next = json['metadata']['next']
-				break if cursor_next.nil?
-
-				params['cursor'] = cursor_next
-			end
-
-			purchases
-		end
-=end
 
 		private
 
@@ -172,9 +120,5 @@ module Luca
 				'Content-Type' => 'application/json'
 			}
 		end
-
-		def ua
-		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:92.0) Gecko/20100101 Firefox/92.0'
-		end		
 	end
 end
